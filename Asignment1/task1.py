d4370737d1d5756d3e737510f01650dc812d4549
Ajeet Gupta <229371738+gajit9147-dev@users.noreply.github.com>
2026-01-30 07:51:14 +0530

    first Asignment

---
+++ Asignment1/task1.py
@@ -0,0 +1,14 @@
+num1= input("Enter the first number:")
+num2= input("Enter the second number:")
+
+sum = int(num1)+int(num2)
+print("Addition:", sum)
+
+diff = int(num1)-int(num2)
+print("Subtraction:", diff)
+
+prod = int(num1)*int(num2)
+print("Multiplication:", prod)
+
+quot = int(num1)/int(num2)
+print("Division:", quot)
---
+++ Asignment1/task2.py
@@ -0,0 +1,4 @@
+name= input("Enter first your name: ")
+lastName= input("Enter your last name: ")
+
+print("Hello, " + name + " " + lastName + "! Welcome to the python program.")
