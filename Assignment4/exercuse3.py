4073439c243fc36cf433da50c73fb129896b2896
gajit9147 <gajit9147@gmail.com>
2026-02-17 17:57:03 +0530

    number count

--- Assignment4/exercuse3.py
+++ Assignment4/exercuse3.py
@@ -4,3 +4,13 @@ with open("numbers.txt","w") as file:
 with open("numbers.txt", "r") as file:
     content = file.read()
     print(content)
+
+# Program to count total characters in a file
+
+file_name = "numbers.txt"
+
+with open(file_name, "r") as file:
+    content = file.read()
+    char_count = len(content)
+
+print("Total number of characters in the file:", char_count)
