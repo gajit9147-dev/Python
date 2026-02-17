0e057c77a1998df9d4ee651d9584fe6302bcc18c
Ajeet Gupta <229371738+gajit9147-dev@users.noreply.github.com>
2026-02-17 17:31:35 +0530

    Append

--- Assignment4/exercise2.py
+++ Assignment4/exercise2.py
@@ -3,3 +3,10 @@
 with open("myname.txt", "r") as file:
     content = file.read()
     print(content)
+
+#Write a program to append a new line to an existing file.
+
+with open("myname.txt", "a") as file:
+    file.write("\n this is my first append operation in python.")
+    print("Data successfully appended to myname.txt.")
+
